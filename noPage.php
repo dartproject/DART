cb27af760acfefbd9d52f4e343aceca9d899390e
Bolek Kurowski <bkurowsk@andrew.cmu.edu>
2011-06-20 12:30:51 -0800

    Fix CRLF

--- noPage.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo "<br><br><font size=4>Sorry, no page yet for this standard. Please close this window.</font>";
-
-?>
--- sfa/simpleplot.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'phplot.php';
-$plot =& new PHPlot();
-$data = array(array('', 0, 0), array('', 1, 9));
-$plot->SetDataValues($data);
-$plot->SetDataType('data-data');
-$plot->DrawGraph();
-?>
