c77465e9e7fb0674a725c957079ca7a6dfffae52
Cristina Melo <csikvand@gmail.com>
2011-06-21 09:43:53 -0800

    Adding merged function and config files

---
+++ lib/machine.config.php
@@ -0,0 +1,21 @@
+<?php
+
+// location of root url
+   //$rootURL = "https://dart.bssd.org";
+   $rootURL = "http://localhost:8888/DART";
+   $rootPATH = dirname(__FILE__) . "/../";
+   $imageURL = $rootURL . "/images/";
+   $logoURL = $rootURL . "/images/SchoolLogos/";
+   $pictureURL = $rootURL . "/images/student/" ;
+   $pictDIR = "/home/dartdb/public_html/images/student/";
+   $exportDIR = "/Applications/MAMP/htdocs/DART/exp/";
+
+// Database inforamtion
+   $dbuser = "dart_user"; 
+   $dbpassword = "dart_psw"; 
+   $dbname = "dartdb_dartdb";
+   $dbhose = "localhost";
+
+// pdftk
+	$pdftk = '/usr/bin/pdftk';
+?>
